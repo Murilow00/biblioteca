ccc93da86da1d2cd83d9718749d657d4dc2491b3
Pedro Escapalete <pedroescapalete@gmail.com>
2026-03-30 15:38:32 -0300

    ft: finaliza a route de cliente

---
+++ src/routes/clienteRoute.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as controller from '../controllers/clienteController.js';
+
+const router = Router();
+
+router.post('/clientes', controller.criar);
+router.get('/clientes', controller.buscarTodos);
+router.get('/clientes/:id', controller.buscarPorId);
+router.put('/clientes/:id', controller.atualizar);
+router.delete('/clientes/:id', controller.desativar);
+
+export default router;
