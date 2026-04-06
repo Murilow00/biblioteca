5e9e3b356a712b9c8cd1cabd823222fd05217ab6
Gustavo Victor <ferreirag930@yahoo.com>
2026-04-06 09:33:12 -0300

    api-key

--- src/routes/fotoRoute.js
+++ src/routes/fotoRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 import * as controller from '../controllers/fotoController.js';
 import { upload } from '../utils/fotoHelper.js'
 
+
 const router = express.Router();
 
 router.post('/:id/foto', upload.single('foto'), controller.uploadFoto);
